579b59bdf2fa4cf905a2fe46d14c39569e3d5a6c
YuriBragaB <yuribragab1@gmail.com>
2026-06-19 01:16:25 -0300

    primeiro script

---
+++ projeto1/main.cpp
@@ -0,0 +1,12 @@
+// igual ao C a função principal é a main
+// cout = abreviação para saida em C (outpot)
+// tem que colocar quando um função é padrão std(standation)
+// precisa pegar as funções com include, pegar as bibliotecas
+
+
+#include <iostream> // i = input, o = outpot, stream = fluxo
+using namespace std; // não precisa mais do std::, pois essa célula diz que vai ser utilizado váriasa funções do std
+
+int main () {
+    cout << "Bem vindo ao jogo do número secreto!" << endl; // endl = en line, finaliza a linha, já que ele é da biblioteca de C++, é colocado std(standation(iniciar))
+}
